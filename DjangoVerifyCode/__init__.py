29862995d0ac5d1ce33b96f9c2e571169e444db5
TY@MacBook Pro <admin@pythoner.net>
2013-09-22 10:49:37 +0800

    update

--- DjangoVerifyCode/__init__.py
+++ DjangoVerifyCode/__init__.py
@@ -4,6 +4,8 @@ import Image,ImageDraw,ImageFont,random,StringIO
 import os
 from math import ceil
 
+__version__ = '0.2.5',  
+
 current_path = os.path.normpath(os.path.dirname(__file__))
 
 class Code(object):
