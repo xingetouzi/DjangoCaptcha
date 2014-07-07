3af7bbcd079a1fe4be67bcfbd258df2c363b80e7
TY@MacBook Pro <admin@pythoner.net>
2014-07-07 14:16:44 +0800

    v2.0.11 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='DjangoCaptcha',  
       author='TY(pythoner.net)',  
       author_email='tianyu0915@gmail.com',  
-      version='0.2.10',  
+      version='0.2.11',  
       description='在django中生成英文/数字公式验证码',  
       keywords ='django verify code 验证码',
       url='http://github.com/tianyu0915/DjangoCaptcha',  
