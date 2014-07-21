36ee1558b1de1f95a18035a9cfe0ea117dcea726
TY@MacBook Pro <admin@pythoner.net>
2014-07-21 18:02:01 +0800

    update package infomation

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #encoding:utf-8
 from distutils.core import setup  
 setup(name='DjangoCaptcha',  
-      author='TY(pythoner.net)',  
+      author='TY(pythoner.org)',  
       author_email='tianyu0915@gmail.com',  
-      version='0.2.11',  
+      version='0.2.12',  
       description='在django中生成英文/数字公式验证码',  
-      keywords ='django verify code 验证码',
+      keywords ='django captcha 验证码',
       url='http://github.com/tianyu0915/DjangoCaptcha',  
       packages=['DjangoCaptcha'],  
       install_requires=['pillow'],
