5023cd0d9b0b13c35ad9e52a840468230d551e4d
TY@MacBook Pro <admin@pythoner.net>
2015-03-05 16:38:48 +0800

    跟新版本号：0.2.13，并发布到pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='DjangoCaptcha',  
       author='TY(pythoner.org)',  
       author_email='tianyu0915@gmail.com',  
-      version='0.2.12',  
+      version='0.2.13',  
       description='在django中生成英文/数字公式验证码',  
       keywords ='django captcha 验证码',
       url='http://github.com/tianyu0915/DjangoCaptcha',  
